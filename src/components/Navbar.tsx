29fb6ecf82aee2b4453062bc66cc82c41eaad0b5
JereNwa <jnwaokolo@icloud.com>
2025-10-13 18:27:55 +0100

    Made it so that the navbar links don't have underlines on hover.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <nav className="w-full py-4 px-8 flex items-center justify-between bg-foreground dark:bg-foreground shadow-md text-[#f4f4f4]">
       <div className="text-xl font-bold">
-        <Link href="/" className="font-alt">TRANSFORMATION PLACE</Link>
+        <Link href="/" className="font-alt hover:no-underline">TRANSFORMATION PLACE</Link>
         </div>
-        <div className="space-x-4 font-sans">
-            <Link href="/about" className="hover:underline">About</Link>
-            <Link href="/our-mission" className="hover:underline">Our Mission</Link>
-            <Link href="/events" className="hover:underline">Events</Link>
-            <Link href="/services" className="hover:underline">Services</Link>
-            <Link href="/contact" className="hover:underline">Contact</Link>
+        <div className="space-x-4 font-sans hover:no-underline">
+            <Link href="/about">About</Link>
+            <Link href="/our-mission">Our Mission</Link>
+            <Link href="/events">Events</Link>
+            <Link href="/services">Services</Link>
+            <Link href="/contact">Contact</Link>
         </div>
     </nav>
   );
