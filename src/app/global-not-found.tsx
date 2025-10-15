c4902a41cf35b826d1152da82384e2e2b4800273
JereNwa <jnwaokolo@icloud.com>
2025-10-15 21:30:14 +0100

    Changed header font in 404 page to Playfair Display.

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -1,9 +1,11 @@
 // Import global styles and fonts
 import './globals.css'
 import { Source_Sans_3 } from 'next/font/google'
+import { Playfair_Display } from 'next/font/google'
 import type { Metadata } from 'next'
  
 const source_sans_3 = Source_Sans_3({ subsets: ['latin'] })
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
  
 export const metadata: Metadata = {
   title: '404 - Page Not Found',
@@ -12,10 +14,10 @@ export const metadata: Metadata = {
  
 export default function GlobalNotFound() {
   return (
-    <html lang="en" className={source_sans_3.className}>
+    <html lang="en" className="">
       <body>
-        <h1 className="text-center font-bold text-5xl mt-6">404 - Page Not Found</h1>
-        <p className="text-center mt-6">This page does not exist.<br/>
+        <h1 className={`text-center font-bold text-5xl mt-6 ${playfairDisplay.className}`}>404 - Page Not Found</h1>
+        <p className={`text-center mt-6 ${source_sans_3.className}`}>This page does not exist.<br/>
         <a href="/" className="text-center mt-6 text hover:underline">Go back home</a></p>
       </body>
     </html>
