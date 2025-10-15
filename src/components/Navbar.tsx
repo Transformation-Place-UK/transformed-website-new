95d71d4aeb65de5d182d4047ad280e238b5245e5
JereNwa <jnwaokolo@icloud.com>
2025-10-15 21:41:43 +0100

    Cleaning up code a little.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import React from "react";
 import { Menu, X } from "lucide-react";
 
-
-
 const Navbar = () => {
 
   const [isOpen, setIsOpen] = React.useState(false);
