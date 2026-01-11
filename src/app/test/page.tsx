84ec38455e1f7b99a636e7a568e86d4f099b0af7
Jeremiah N <jnwaokolo@icloud.com>
2026-01-11 12:11:39 +0000

    Edited test page.

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,4 @@
-import * as motion from "motion/react-client"
+import * as motion from "motion/react-client";
 
 export default function TestPage() {
   return (
@@ -7,7 +7,7 @@ export default function TestPage() {
         className="w-32 h-32 bg-blue-500 flex items-center justify-center text-white font-bold"
         animate={{ rotate: 360 }}
         transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-      > Rotating Box</motion.div>
+      >Rotating Box</motion.div>
     </div>
   );
 }
